2c64a7eec42675dbc7ea970b4ebed3b3f5e33815
Josh Crawford <joshua@sgroup.com.au>
2023-04-20 13:42:59 +1000

    Debug

--- src/web/assets/app/src/js/components/AppHeader.vue
+++ src/web/assets/app/src/js/components/AppHeader.vue
@@ -2,11 +2,13 @@
     <div id="page-title" class="flex">
         <h1 class="screen-title" :title="breadcrumb">{{ breadcrumb }}</h1>
 
-        &nbsp;&nbsp;
-        <a @click.prevent="navigate('/')">Index</a>&nbsp;&nbsp;&nbsp;&nbsp;
-        <a @click.prevent="navigate('/import/configure/zen-import-230322_123909.zip')">Configure</a>&nbsp;&nbsp;&nbsp;&nbsp;
-        <a @click.prevent="navigate('/import/review/zen-import-230322_123909.zip')">Review</a>&nbsp;&nbsp;&nbsp;&nbsp;
-        <a @click.prevent="navigate('/import/run/zen-import-230322_123909.zip')">Run</a>
+        <div v-if="$isDebug">
+            &nbsp;&nbsp;
+            <a @click.prevent="navigate('/')">Index</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a @click.prevent="navigate('/import/configure/zen-test.zip')">Configure</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a @click.prevent="navigate('/import/review/zen-test.zip')">Review</a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a @click.prevent="navigate('/import/run/zen-test.zip')">Run</a>
+        </div>
     </div>
 
     <transition name="page-fade" mode="out-in">
