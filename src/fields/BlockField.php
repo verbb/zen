0c6b8911f1cd4c71b988a140ad5f2680980ebf3f
Josh Crawford <joshua@sgroup.com.au>
2023-09-14 15:13:51 +1000

    Refactor Matrix/etc common functions

--- src/fields/BlockField.php
+++ src/fields/BlockField.php
@@ -12,7 +12,7 @@ use craft\base\FieldInterface;
 use craft\elements\MatrixBlock;
 use craft\fields\Matrix as MatrixField;
 
-class Matrix extends BlockField
+class BlockField extends ZenField
 {
     // Static Methods
     // =========================================================================
@@ -102,6 +102,96 @@ class Matrix extends BlockField
         }
 
         return $blocks;
+    }
+
+    public static function getFieldForPreview(FieldInterface $field, ElementInterface $element, string $type): void
+    {
+        $fieldsService = Zen::$plugin->getFields();
+
+        $value = $element->getFieldValue($field->handle);
+
+        foreach ($value->all() as $block) {
+            // Ensure all sub-fields are prepped for preview
+            foreach ($fieldsService->getCustomFields($block->getType()) as $subField) {
+                $fieldsService->getFieldForPreview($subField, $block, $type);
+            }
+        }
+
+        parent::getFieldForPreview($field, $element, $type);
+    }
+
+    public static function beforeElementImport(FieldInterface $field, ElementInterface $element): bool
+    {
+        $fieldsService = Zen::$plugin->getFields();
+
+        $value = $element->getFieldValue($field->handle);
+
+        foreach ($value->all() as $block) {
+            // Ensure we trigger inner fields' `beforeElementImport()`
+            $fieldsService->beforeElementImport($block);
+        }
+
+        return parent::beforeElementImport($field, $element);
+    }
+
+    public static function afterElementImport(FieldInterface $field, ElementInterface $element): void
+    {
+        $fieldsService = Zen::$plugin->getFields();
+
+        $value = $element->getFieldValue($field->handle);
+
+        foreach ($value->all() as $block) {
+            // Ensure we trigger inner fields' `beforeElementImport()`
+            $fieldsService->beforeElementImport($block);
+        }
+
+        parent::afterElementImport($field, $element);
+    }
+
+    public static function handleValueForDiff(FieldInterface $field, mixed &$oldValue, mixed &$newValue): ?array
+    {
+        // By default, the ElementDiffer at the element-compare level won't be recursive into field data.
+        // But for Matrix, we want to show the diff between blocks, so run our own diffs.
+        $differ = new ElementDiffer();
+        $diffs = [];
+
+        if (is_array($oldValue) && is_array($newValue)) {
+            foreach (ArrayHelper::getAllKeys($oldValue, $newValue) as $nestedKey) {
+                $oldValueNested = $oldValue[$nestedKey] ?? [];
+                $newValueNested = $newValue[$nestedKey] ?? [];
+
+                $diff = $differ->doDiff($oldValueNested, $newValueNested);
+
+                if ($diff !== null) {
+                    $diffs[$nestedKey] = $diff;
+                }
+            }
+        }
+
+        return array_filter($diffs);
+    }
+
+    public static function handleValueForDiff(FieldInterface $field, mixed &$oldValue, mixed &$newValue): ?array
+    {
+        // By default, the ElementDiffer at the element-compare level won't be recursive into field data.
+        // But for Matrix, we want to show the diff between blocks, so run our own diffs.
+        $differ = new ElementDiffer();
+        $diffs = [];
+
+        if (is_array($oldValue) && is_array($newValue)) {
+            foreach (ArrayHelper::getAllKeys($oldValue, $newValue) as $nestedKey) {
+                $oldValueNested = $oldValue[$nestedKey] ?? [];
+                $newValueNested = $newValue[$nestedKey] ?? [];
+
+                $diff = $differ->doDiff($oldValueNested, $newValueNested);
+
+                if ($diff !== null) {
+                    $diffs[$nestedKey] = $diff;
+                }
+            }
+        }
+
+        return array_filter($diffs);
     }
 
 }
--- src/fields/Neo.php
+++ src/fields/Neo.php
@@ -14,7 +14,7 @@ use benf\neo\elements\Block as NeoBlock;
 use benf\neo\Field as NeoField;
 use benf\neo\Plugin as NeoPlugin;
 
-class Neo extends ZenField
+class Neo extends BlockField
 {
     // Static Methods
     // =========================================================================
@@ -105,73 +105,6 @@ class Neo extends ZenField
         }
 
         return $blocks;
-    }
-
-    public static function getFieldForPreview(FieldInterface $field, ElementInterface $element, string $type): void
-    {
-        $fieldsService = Zen::$plugin->getFields();
-
-        $value = $element->getFieldValue($field->handle);
-
-        foreach ($value->all() as $block) {
-            // Ensure all sub-fields are prepped for preview
-            foreach ($fieldsService->getCustomFields($block->getType()) as $subField) {
-                $fieldsService->getFieldForPreview($subField, $block, $type);
-            }
-        }
-
-        parent::getFieldForPreview($field, $element, $type);
-    }
-
-    public static function beforeElementImport(FieldInterface $field, ElementInterface $element): bool
-    {
-        $fieldsService = Zen::$plugin->getFields();
-
-        $value = $element->getFieldValue($field->handle);
-
-        foreach ($value->all() as $block) {
-            // Ensure we trigger inner fields' `beforeElementImport()`
-            $fieldsService->beforeElementImport($block);
-        }
-
-        return parent::beforeElementImport($field, $element);
-    }
-
-    public static function afterElementImport(FieldInterface $field, ElementInterface $element): void
-    {
-        $fieldsService = Zen::$plugin->getFields();
-
-        $value = $element->getFieldValue($field->handle);
-
-        foreach ($value->all() as $block) {
-            // Ensure we trigger inner fields' `beforeElementImport()`
-            $fieldsService->beforeElementImport($block);
-        }
-
-        parent::afterElementImport($field, $element);
-    }
-
-    public static function handleValueForDiff(FieldInterface $field, mixed &$oldValue, mixed &$newValue): ?array
-    {
-        // By default, the ElementDiffer at the element-compare level won't be recursive into field data.
-        // But for Matrix, we want to show the diff between blocks, so run our own diffs.
-        $differ = new ElementDiffer();
-        $diffs = [];
-
-        if (is_array($oldValue) && is_array($newValue)) {
-            foreach (ArrayHelper::getAllKeys($oldValue, $newValue) as $nestedKey) {
-                $oldValueNested = $oldValue[$nestedKey] ?? [];
-                $newValueNested = $newValue[$nestedKey] ?? [];
-
-                $diff = $differ->doDiff($oldValueNested, $newValueNested);
-
-                if ($diff !== null) {
-                    $diffs[$nestedKey] = $diff;
-                }
-            }
-        }
-
-        return array_filter($diffs);
     }
 
 }
